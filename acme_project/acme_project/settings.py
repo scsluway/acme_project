faa46a380e530bc062c93908520e3c0b13ee328e
Muhammad Saidov <saidov.m.05.01@gmail.com>
2024-04-20 20:36:40 +0300

    qweradsf

--- acme_project/acme_project/settings.py
+++ acme_project/acme_project/settings.py
@@ -6,7 +6,10 @@ SECRET_KEY = 'django-insecure-m&$lzdzkutvrbr5vt=jpm)7#g7cken_tk%($ty+w902n7wb#=e
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    ]
 
 INSTALLED_APPS = [
     'birthday.apps.BirthdayConfig',
@@ -18,6 +21,7 @@ INSTALLED_APPS = [
     'django_bootstrap5',
     'django.contrib.staticfiles',
     'pages.apps.PagesConfig',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
@@ -28,6 +32,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'acme_project.urls'
@@ -89,3 +94,18 @@ STATIC_URL = '/static/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Подключаем бэкенд filebased.EmailBackend:
+EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
+# Указываем директорию, в которую будут сохраняться файлы писем:
+EMAIL_FILE_PATH = BASE_DIR / 'sent_emails'
+
+LOGIN_REDIRECT_URL = 'pages:homepage'
+
+LOGIN_URL = 'login'
+
+CSRF_FAILURE_VIEW = 'core.views.csrf_failure'
+
+INTERNAL_IPS = [
+    '127.0.0.1',
+]
--- acme_project/birthday/forms.py
+++ acme_project/birthday/forms.py
@@ -1,8 +1,9 @@
 # birthday/forms.py
 from django import forms
 from django.core.exceptions import ValidationError
+from django.core.mail import send_mail
 # Импортируем класс модели Birthday.
-from .models import Birthday
+from .models import Birthday, Congratulation
 
 
 BEATLES = {'Джон Леннон', 'Пол Маккартни', 'Джордж Харрисон', 'Ринго Старр'}
@@ -17,6 +18,7 @@ class BirthdayForm(forms.ModelForm):
         # Указываем модель, на основе которой должна строиться форма.
         model = Birthday
         # Указываем, что надо отобразить все поля.
+        exclude = ('author',)
         fields = '__all__'
         widgets = {
             'birthday': forms.DateInput(attrs={'type': 'date'}),
@@ -25,16 +27,33 @@ class BirthdayForm(forms.ModelForm):
     def clean_first_name(self):
         # Получаем значение имени из словаря очищенных данных.
         first_name = self.cleaned_data['first_name']
-        # Разбиваем полученную строку по пробелам 
+        # Разбиваем полученную строку по пробелам
         # и возвращаем только первое имя.
         return first_name.split()[0]
 
     def clean(self):
-        # Вызов родительского метода clean.
         super().clean()
         first_name = self.cleaned_data['first_name']
         last_name = self.cleaned_data['last_name']
         if f'{first_name} {last_name}' in BEATLES:
+            # Отправляем письмо, если кто-то представляется
+            # именем одного из участников Beatles.
+            send_mail(
+                subject='Another Beatles member',
+                message=(
+                    f'{first_name} {last_name} пытался опубликовать запись!'
+                ),
+                from_email='birthday_form@acme.not',
+                recipient_list=['admin@acme.not'],
+                fail_silently=True,
+            )
             raise ValidationError(
                 'Мы тоже любим Битлз, но введите, пожалуйста, настоящее имя!'
             )
+
+
+class CongratulationForm(forms.ModelForm):
+
+    class Meta:
+        model = Congratulation
+        fields = ('text',)
